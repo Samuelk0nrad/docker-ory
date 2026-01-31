1158c4a579bcd07340268b8dca71ffc139e6bca0
SamuelK0nrad <samuel.ko702@gmail.com>
2026-01-31 16:01:01 +0100

    add example next js application with ory elements

---
+++ example-next-app/next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  //async rewrites() {
+  //  return [
+  //    {
+  //      source: "/self-service/:path*",
+  //      destination: "http://localhost:5545/self-service/:path*"
+  //    },
+  //    {
+  //      source: "/sessions/:path*",
+  //      destination: "http://localhost:5545/sessions/:path*"
+  //    }
+  //  ];
+  //}
+};
+
+
+export default nextConfig;
