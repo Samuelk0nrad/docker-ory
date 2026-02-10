85525c4b78ac264e6fe621b777e91f2280b52b16
SamuelK0nrad <samuel.ko702@gmail.com>
2026-02-10 19:27:04 +0100

    fix hydra basic impl

--- example-next-app/app/page.tsx
+++ example-next-app/app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
-  const { session, user, loading, isLoggedIn, error } = useSession();
+  const { session, user, isLoggedIn } = useSession();
 
   if (isLoggedIn) {
     return (
@@ -58,7 +58,6 @@ export default function Home() {
           <Link
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="/auth/login"
-            target="_blank"
             rel="noopener noreferrer"
           >
             Sign In
@@ -66,7 +65,6 @@ export default function Home() {
           <Link
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="/auth/registration"
-            target="_blank"
             rel="noopener noreferrer"
           >
             Sign Up
--- example-next-app/next.config.ts
+++ example-next-app/next.config.ts
@@ -5,11 +5,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/.ory/kratos/:path*",
-        destination: "http://localhost:5545/:path*"
+        destination: process.env.KRATOS_PUBLIC_BASE_URL + "/:path*" // kratos public API server
       },
       {
         source: "/api/.ory/hydra/:path*",
-        destination: "http://localhost:5545/:path*"
+        destination: process.env.HYDRA_PUBLIC_BASE_URL + "/:path*" // hydra public API server
       }
     ];
   }
